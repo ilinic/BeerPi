d49bda9aa2cc5988f48c006b6d70b0c9153d2399
ilinic <ilinic@mail.ru>
2020-02-13 16:44:21 +0300

    c

--- execute.sh
+++ execute.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo java -jar ./BeerPi_jar/BeerPi.jar -enableassertions -Dswing.aatext=TRUE -Dawt.useSystemAAFontSettings=on -Dfile.encoding=utf-8
+sudo java -jar ./BeerPi_jar/BeerPi.jar -enableassertions
