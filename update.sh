c385458defdbb1554a317c7034270a3956f74811
ilinic <ilinic@mail.ru>
2020-03-02 22:40:54 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ sudo chown -R $USER:$USER "$(git rev-parse --show-toplevel)/.git"
 sudo git fetch --all
 sudo git reset --hard origin/master
 echo ""
-sudo read -p "Please press enter to continue..." nothing 
+read -p "Please press enter to continue..." nothing 
 
