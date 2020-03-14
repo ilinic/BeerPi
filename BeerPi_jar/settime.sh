a51a8ed274842a8d86fe89bed23d0c7e2d516e23
ilinic <ilinic@mail.ru>
2020-03-15 02:07:33 +0300

    Update settime.sh

--- BeerPi_jar/settime.sh
+++ BeerPi_jar/settime.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-sudo timedatectl set-timezone $2
-sudo date -s @$1
-sudo hwclock --systohc
+sudo timedatectl set-timezone Europe/Moscow
+cd /home/pi/Desktop/compiledBeerPi/BeerPi_jar/
+sudo ntpdate -u ntp.ubuntu.com
+tval="$(date +%s)"
+sudo java -jar set_maple_clock.jar ttyAMA0 $tval
+read -p "Please press enter to continue..." nothing 
