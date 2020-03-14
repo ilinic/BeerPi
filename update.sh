657aa3524477c867f3c0382c9c93365164114d89
ilinic <ilinic@mail.ru>
2020-03-15 01:26:26 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,8 @@
 sudo chown -R $USER:$USER "$(git rev-parse --show-toplevel)/.git"
 sudo git fetch --all
 sudo git reset --hard origin/master
+sudo chmod a+x *.sh
+sudo chmod a+x BeerPi_jar/*.sh
 echo ""
 read -p "Please press enter to continue..." nothing 
 
