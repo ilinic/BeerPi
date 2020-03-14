2f6f372ad43aa7641da753e6ea7d63de4c5acdec
ilinic <ilinic@mail.ru>
2020-03-15 01:22:44 +0300

    Update gettime.sh

--- BeerPi_jar/gettime.sh
+++ BeerPi_jar/gettime.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo timedatectl set-timezone Europe/Moscow
-tval="$(sudo java -jar /home/pi/Desktop/compiledBeerPi/BeerPi_jar/get_maple_clock.jar ttyAMA0)"
+cd /home/pi/Desktop/compiledBeerPi/BeerPi_jar/
+tval="$(java -jar get_maple_clock.jar ttyAMA0)"
 sudo date -s @$tval
 read -p "Please press enter to continue..." nothing 
