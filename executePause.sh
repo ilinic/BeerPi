52fc8fce39cf04e1a20ba7978d208fc97480f7cb
ilinic <ilinic@mail.ru>
2020-07-24 22:05:15 +0300

    Update executePause.sh

--- executePause.sh
+++ executePause.sh
@@ -6,5 +6,5 @@ sudo chmod a+x *.sh
 sudo chmod a+x BeerPi_jar/*.sh
 sudo xset s off
 
-sudo java -jar --module-path /usr/share/openjfx/lib --add-modules javafx.* ./BeerPi_jar/BeerPi.jar -ea
+sudo java -jar --module-path /usr/share/openjfx/lib --add-modules javafx.controls, javafx.graphics ./BeerPi_jar/BeerPi.jar -ea
 read -p "Please press enter to continue..." nothing 
