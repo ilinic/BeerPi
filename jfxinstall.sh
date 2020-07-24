7ab25918f451b6c223cc47482480bd97564df2c5
ilinic <ilinic@mail.ru>
2020-07-24 21:05:22 +0300

    c

--- jfxinstall.sh
+++ jfxinstall.sh
@@ -1,5 +1,6 @@
 cd /home/pi
-wget http://download.bell-sw.com/java/14+36/bellsoft-jdk14+36-linux-arm32-vfp-hflt-full.deb
-sudo apt-get install ./bellsoft-jdk14+36-linux-arm32-vfp-hflt-full.deb
-sudo update-alternatives --config javac
-sudo update-alternatives --config java
+#wget http://download.bell-sw.com/java/14+36/bellsoft-jdk14+36-linux-arm32-vfp-hflt-full.deb
+#sudo apt-get install ./bellsoft-jdk14+36-linux-arm32-vfp-hflt-full.deb
+sudo apt-get remove ./bellsoft-jdk14+36-linux-arm32-vfp-hflt-full.deb
+#sudo update-alternatives --config javac
+#sudo update-alternatives --config java
