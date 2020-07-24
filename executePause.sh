c88deab37eb01c71bb4733f9d02572359a99a638
ilinic <ilinic@mail.ru>
2020-07-25 02:59:48 +0300

    c

--- executePause.sh
+++ executePause.sh
@@ -3,14 +3,10 @@
 
 cd /home/pi/Desktop/compiledBeerPi/
 
-sudo chown -R $USER:$USER "$(git rev-parse --show-toplevel)/.git"
-sudo git fetch --all
-sudo git reset --hard origin/master
-
 sudo chmod a+x *.sh
 sudo chmod a+x BeerPi_jar/*.sh
 sudo xset s off
 
 #sudo nice -20 java -Dprism.order=sw -Xmx2G -jar --module-path /usr/share/openjfx/lib --add-modules ALL-MODULE-PATH ./BeerPi_jar/BeerPi.jar -ea
-sudo nice -20 java -Xms=1G -Xmx=2G -jar ./BeerPi_jar/BeerPi.jar -ea
+sudo nice -20 java -Xms=500M -jar ./BeerPi_jar/BeerPi.jar -ea
 read -p "Please press enter to continue..." nothing 
