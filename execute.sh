aa0b62d556c858ebdcfec9525d1f1deac5045a81
ilinic <ilinic@mail.ru>
2020-09-13 14:55:50 +0300

    c

--- execute.sh
+++ execute.sh
@@ -8,5 +8,5 @@ sudo xset s off
 
 #while :
 #do
-	sudo nice -20 java -Xms1G -jar ./BeerPi_jar/BeerPi.jar -ea --add-opens java.base/java.lang=ALL-UNNAMED
+	sudo nice -20 java -Xms1G -jar /home/pi/Desktop/BeerPi/BeerPi_jar/BeerPi.jar -ea --add-modules ALL-MODULE-PATH --add-opens java.base/java.lang=ALL-UNNAMED
 #done
--- executePause.sh
+++ executePause.sh
@@ -9,5 +9,5 @@ sudo xset s off
 
 #sudo nice -20 java -Dprism.order=sw -Xmx2G -jar --module-path /usr/share/openjfx/lib --add-modules ALL-MODULE-PATH ./BeerPi_jar/BeerPi.jar -ea
 #sudo nice -20 java -Xms=1G -Xmx=2G -jar ./BeerPi_jar/BeerPi.jar -ea
-sudo nice -20 java -Xms1G -jar ./BeerPi_jar/BeerPi.jar -ea --add-opens java.base/java.lang=ALL-UNNAMED
+sudo nice -20 java -Xms1G -jar /home/pi/Desktop/BeerPi/BeerPi_jar/BeerPi.jar -ea --add-modules ALL-MODULE-PATH --add-opens java.base/java.lang=ALL-UNNAMED
 read -p "Please press enter to continue..." nothing 
