bef4f7b9d517d8b82848021c7398d42f45646c07
ilinic <ilinic@mail.ru>
2020-09-14 02:51:51 +0300

    c

--- execute.sh
+++ execute.sh
@@ -8,5 +8,6 @@ sudo xset s off
 
 #while :
 #do
-	sudo nice -20 java -Xms1G -jar /home/pi/Desktop/BeerPi/BeerPi_jar/BeerPi.jar -ea --add-modules ALL-MODULE-PATH --add-opens java.base/java.lang=ALL-UNNAMED
+#sudo ionice -c 1 -n 0 java -Xms1G -jar /home/pi/Desktop/BeerPi/BeerPi_jar/BeerPi.jar -ea --add-modules ALL-MODULE-PATH --add-opens java.base/java.lang=ALL-UNNAMED
+	sudo ionice -c 1 -n 0 java -jar /home/pi/Desktop/BeerPi/BeerPi_jar/BeerPi.jar -ea --add-modules ALL-MODULE-PATH --add-opens java.base/java.lang=ALL-UNNAMED
 #done
