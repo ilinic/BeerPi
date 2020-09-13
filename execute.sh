8095cc1c9083f7698442e8e47ef421b6fe3b385b
ilinic <ilinic@mail.ru>
2020-09-13 14:20:03 +0300

    c

--- execute.sh
+++ execute.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /home/pi/Desktop/compiledBeerPi/
+cd /home/pi/Desktop/BeerPi/
 
 sudo chmod a+x *.sh
 sudo chmod a+x BeerPi_jar/*.sh
@@ -8,5 +8,5 @@ sudo xset s off
 
 #while :
 #do
-	sudo nice -20 java -Xms1G -jar ./BeerPi_jar/BeerPi.jar -ea
+	sudo nice -20 java -Xms1G -jar ./BeerPi_jar/BeerPi.jar -ea --add-opens java.base/java.lang=ALL-UNNAMED
 #done
--- executePause.sh
+++ executePause.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-cd /home/pi/Desktop/compiledBeerPi/
+cd /home/pi/Desktop/BeerPi/
 
 sudo chmod a+x *.sh
 sudo chmod a+x BeerPi_jar/*.sh
@@ -9,5 +9,5 @@ sudo xset s off
 
 #sudo nice -20 java -Dprism.order=sw -Xmx2G -jar --module-path /usr/share/openjfx/lib --add-modules ALL-MODULE-PATH ./BeerPi_jar/BeerPi.jar -ea
 #sudo nice -20 java -Xms=1G -Xmx=2G -jar ./BeerPi_jar/BeerPi.jar -ea
-sudo nice -20 java -Xms1G -jar ./BeerPi_jar/BeerPi.jar -ea
+sudo nice -20 java -Xms1G -jar ./BeerPi_jar/BeerPi.jar -ea --add-opens java.base/java.lang=ALL-UNNAMED
 read -p "Please press enter to continue..." nothing 
