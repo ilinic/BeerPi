9c0368a8b5eece5bafda474e3ae9475f96ed4cce
ilinic <ilinic@mail.ru>
2020-09-16 03:11:40 +0300

    c

--- execute.sh
+++ execute.sh
@@ -9,14 +9,14 @@ sudo xset s noblank
 sudo timedatectl set-ntp false
 sudo setxkbmap -option grp:switch,grp:alt_shift_toggle,grp_led:scroll us,ru
 
-while :
-do
+#while :
+#do
 	sudo ionice -c 1 -n 0 java -jar /home/pi/BeerPi/BeerPi_jar/BeerPi.jar --add-opens java.base/java.lang=ALL-UNNAMED
 
 	if [ $? != 0 ]; then
 		break
 	fi
-done
+#done
 
 sudo timedatectl set-ntp true
 lxpanel --profile LXDE-pi
